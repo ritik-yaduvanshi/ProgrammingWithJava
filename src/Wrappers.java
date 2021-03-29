fe397f34a6d3b546d1780dbca0b1091f6b66a072
CodexRitik <ritik.yadav_cs19@gla.ac.in>
2021-03-30 00:14:16 +0530

    Wrapper Class in Java

---
+++ src/Wrappers.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Wrappers {
+    /*
+    Wrapper Class is used for converting a primitive into Object Datatype
+    and Object into Primitive.
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        //primitive type of value
+        int value = scan.nextInt();
+        long bigValue = scan.nextLong();
+        char ch = scan.next().charAt(0);
+
+        //Converting this data into Object data (It is called AutoBoxing)
+        Integer objValue = new Integer(value);//int to Integer conversion explicitly
+        //You may simple use
+        Integer ObjValue = value;
+        Long BigValue = bigValue;
+        Character character = ch;
+
+        //Object Data
+        Integer num = 50;
+        Character chars = 'B';
+        Long Values = 64854858L;
+        Float num1 = 34.457F;
+
+        ///Converting Object to Primitive (It is Called UnBoxing)
+        int newNumber = num.intValue();//Converting Integer to int explicitly
+        //You may simple use
+        int newNum = num;
+        char chars1 = chars;
+        long values1 = Values;
+        float num2 = num1;
+
+        System.out.println("int Value:\s"+value+"\t"+"Integer Object value:\s"+ObjValue);
+        System.out.println("long Value:\s"+bigValue+"\t"+"Long Object Value:\s"+BigValue);
+        System.out.println("char Value:\s"+ch+"\t"+"Character Object Value:\s"+character);
+    }
+}
