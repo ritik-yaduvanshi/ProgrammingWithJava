4178aaa07e0ad400936867d705345b84f773c5bb
CodexRitik <ritik.yadav_cs19@gla.ac.in>
2021-04-04 14:44:17 +0530

    Relational Operators in Java.

---
+++ src/RelationalOperators.java
@@ -0,0 +1,20 @@
+/*
+Relational Operators is used for find a relation between two or more
+arguments
+1.equal to '=='
+2.Not equal to '!='
+3.Greater then and Greater then equal to '>' '>='
+4.Less then and less then equal to '<' '<='
+ */
+public class RelationalOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("a == b = " + (a == b) );
+        System.out.println("a != b = " + (a != b) );
+        System.out.println("a > b = " + (a > b) );
+        System.out.println("a < b = " + (a < b) );
+        System.out.println("b >= a = " + (b >= a) );
+        System.out.println("b <= a = " + (b <= a));
+    }
+}
