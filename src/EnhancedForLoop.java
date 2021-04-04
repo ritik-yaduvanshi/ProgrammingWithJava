6a3acb2069d1c12c6d55af83dca87074a68673d9
CodexRitik <ritik.yadav_cs19@gla.ac.in>
2021-04-04 16:20:45 +0530

    Enhanced For Loop In Java.

---
+++ src/EnhancedForLoop.java
@@ -0,0 +1,18 @@
+/*
+This is mainly used to traverse collection of elements including arrays.
+ */
+public class EnhancedForLoop {
+    public static void main(String[] args) {
+        int [] numbers = {10, 20, 30, 40, 50};
+        for(int x : numbers ){
+            System.out.print( x );
+            System.out.print(",");
+        }
+        System.out.print("\n");
+        String [] names ={"James", "Larry", "Tom", "Lacy"};
+        for( String name : names ) {
+            System.out.print( name );
+            System.out.print(",");
+        }
+    }
+}
